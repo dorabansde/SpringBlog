7196897dde840c60822a015d85b6b368a6fa77ca
DESKTOP-MNR8ONA\fktls <fktlsrja@gmail.com>
2022-03-29 20:09:56 +0900

    uplode

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.*;
+public class Main {	
+	
+		public static void main(String[] args){
+
+			Scanner sc = new Scanner(System.in);
+					
+			Stack<Integer> st = new Stack<Integer>();
+
+		
+			
+			sc.close();
+		}
+}
