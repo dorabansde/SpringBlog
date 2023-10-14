51e6cf2b3291d31188a785aab4266ee9f11a6dc2
fktlsrja <fktlsrja@gmail.com>
2023-10-14 10:56:53 +0900

    10월 14일 버전

---
+++ JAVA/백엔드 기초책/springboot-devloper/src/main/java/me/jwc/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,50 @@
+package me.jwc.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.jwc.springbootdeveloper.domain.Article;
+import me.jwc.springbootdeveloper.dto.AddArticleRequest;
+import me.jwc.springbootdeveloper.dto.ArticleResponse;
+import me.jwc.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController<article, AricleResponse> {
+
+    private final BlogService blogService;
+
+    // HTTP 메서드가 POST일 때 전달받은 URL과 동일하면 메서드로 매핑
+    @PostMapping("/api/articles")
+
+    // @RequsBody로 요청 본문 값 매핑
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+
+        // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+    @GetMapping("/api/articles")
+    public ResponseEntity<Object> findAllAricles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+    @GetMapping("/api/articles/{id}")
+    // URL 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+}
